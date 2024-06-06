6d2a22edf571a2e4c6a9b84c86d6cf66fb98f731
Mayckol2005 <169004444+Mayckol2005@users.noreply.github.com>
2024-06-05 20:58:31 -0400

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,15 @@
+def suma(num1,num2):
+    suma = (num1 + num2);
+    print(f"Suma: {suma}")
+
+def resta(num1,num2):
+    resta = (num1 - num2);
+    print(f"Resta: {resta}");
+
+def multi(num1,num2):
+    multi = (num1 * num2);
+    print(f"Multiplicacion: {multi}");
+
+def division(num1,num2):
+    division = (num1 / num2);
+    print(f"Division: {division}");
---
+++ principal.py
@@ -0,0 +1,13 @@
+from funciones import * 
+
+num1 = int(input("\nIngrese un número: "))
+num2 = int(input("Ingrese otro número: "))
+
+print()
+
+suma(num1,num2)
+resta(num1,num2)
+multi(num1,num2)
+division(num1,num2)
+
+print()
